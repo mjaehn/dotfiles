ae7e6af80d2a26d90e009ab61e6e41a3100ae0a5
Michael Jähn <mjaehn@ethz.ch>
2024-10-03 13:25:45 +0200

    Add alias for levante

--- aliases.zsh
+++ aliases.zsh
@@ -17,6 +17,9 @@ if [[ "${ZSHRC_HOST}" == "todi" ]]; then
     alias uenv_tools="uenv start --view=modules netcdf-tools/2024:v1-rc1"
     alias uenv_icon="uenv start --view=spack icon-wcp/v1:rc4"
 fi
+if [[ "${ZSHRC_HOST}" == "levante" ]]; then
+    alias st="cd /pool/data/CLMcom/CCLM/reanalyses/ERA5"
+fi
 if [[ "${CLUSTER}" == "iac" ]]; then
     alias ftps="cd /net/iacftp/ftp/pub_read/mjaehn"
 fi
