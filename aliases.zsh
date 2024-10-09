32e49148d198b8ebcc266914c1a16793ad2d1b4a
Michael Jähn <mjaehn@ethz.ch>
2024-10-09 15:47:37 +0200

    add aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,6 @@
 # General aliases
 alias aliases="vi $HOME/.oh-my-zsh/custom/aliases.zsh"
+alias bashconfig="vi ~/.bashrc"
 alias c="clear"
 alias cscskey="cd ~/git/cscs-keys && ./generate-keys.sh"
 alias f="find . -name"
@@ -9,13 +10,18 @@ alias nv="ncview"
 alias ohmyzsh="vi ~/.oh-my-zsh"
 alias scra="cd ${SCRATCH}"
 alias sq="squeue -u mjaehn"
+alias sqw="watch -n 60 squeue -u mjaehn"
 alias vi="vim -p"
 alias zshconfig="vi ~/.zshrc"
 
-# Additional aliases
+# Machine-dependent aliases
 if [[ "${ZSHRC_HOST}" == "todi" ]]; then
     alias uenv_tools="uenv start --view=modules netcdf-tools/2024:v1-rc1"
     alias uenv_icon="uenv start --view=spack icon-wcp/v1:rc4"
+    alias nn="module load netcdf-c/4.9.2 ncview/2.1.9 && echo Loading ncdump and ncview."
+fi
+if [[ "${ZSHRC_HOST}" == "balfrin" ]]; then
+    alias nn="module load netcdf-c/4.8.1-gcc && echo Loading ncdump."
 fi
 if [[ "${ZSHRC_HOST}" == "levante" ]]; then
     alias st="cd /pool/data/CLMcom/CCLM/reanalyses/ERA5"
