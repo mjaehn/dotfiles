d35afc3b28cbce1c67a379a1e0a912a7f30ecd60
Michael Jähn <mjaehn@ethz.ch>
2025-11-07 11:30:05 +0100

    Update netcdf-tools uenv

--- aliases.zsh
+++ aliases.zsh
@@ -29,7 +29,7 @@ sq2() {
 
 # Machine-dependent aliases
 if [[ "${ZSHRC_HOST}" == "santis" ]]; then
-    alias uenv_tools="uenv start --view=modules netcdf-tools/2024:v1"
+    alias uenv_tools="uenv start --view=netcdf netcdf-tools/2025:v1"
     alias uenv_icon="uenv start icon-wcp/v1:rc4"
     alias climtools="uenv start --view=modules /capstor/store/cscs/userlab/cwd01/leclairm/uenvs/images/climtools_25.2_v2.sqfs"
     alias nn="module load netcdf-c/4.9.2 ncview/2.1.9 && echo Loading ncdump and ncview."
